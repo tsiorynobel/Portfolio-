49ac93ad92d32114c02101c80c68d1d0ddffea49
Patrice <nobel.lebonheurdepatride@gmail.com>
2025-12-16 10:03:30 +0100

    Mise à jour du favicon et langue

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -19,7 +19,7 @@ const NavBar = (props) => {
 										: "nav-item"
 								}
 							>
-								<Link to="/">Home</Link>
+								<Link to="/">Acceuil</Link>
 							</li>
 							<li
 								className={
@@ -28,7 +28,7 @@ const NavBar = (props) => {
 										: "nav-item"
 								}
 							>
-								<Link to="/about">About</Link>
+								<Link to="/about">A propos</Link>
 							</li>
 							<li
 								className={
